dc397658cd0bf0ec6d8900fe9d182e2bfa217a5c
Yash Chouriya <yash.chouriya@oodles.io>
2023-05-10 20:59:25 +0530

    init added flask backend and client folder containing js script which can be embed on other sites

---
+++ server/README.md
@@ -0,0 +1,94 @@
+# chat-gpt-celery-flask
+
+A sample implementation of a chatbot using Flask, Celery, and GPT-3.
+
+## Overview
+
+This repository demonstrates how to create a simple chatbot by combining the power of Flask, Celery, and GPT-3. Flask is used as the web framework to handle HTTP requests and responses, Celery is used as the task queue to run GPT-3 requests asynchronously, and GPT-3 is used as the language generation model to generate text based on the prompts provided by the user.
+
+## Requirements
+
+- Python 3.7 or later
+- Flask
+- Celery
+- Redis (or any other message broker supported by Celery)
+- OpenAI API Key (to access GPT-3)
+
+## Usage
+
+1. Clone the repository:
+   ```
+   git clone git@github.com:yashChouriya/bubblez-ai-instantly.git
+   ```
+2. Navigate to server folder:
+   ```
+   cd server
+   ```
+3. Install the dependencies:
+   ```
+   pip install -r requirements.txt
+   ```
+4. Replace `example.env` to `.env` and change `YOUR_API_KEY` in `app.py` with your OpenAI API key.
+5. Start the Redis server:
+   ```
+   redis-server
+   ```
+6. Start the Celery worker:
+   ```
+   celery -A app.celery worker --loglevel=info
+   ```
+7. Start the Flask app:
+   ```
+   flask run
+   ```
+8. Send a POST request to `http://localhost:5000/chat` with a JSON body containing the prompt, for example:
+   ```
+   { "prompt": "What is the capital of France?" }
+   ```
+9. You will receive a JSON response containing the task id, for example:
+   ```
+   { "task_id": "8d5f0b5d-a5e5-4b8f-b5f5-5a5a5a5a5a5a" }
+   ```
+10. Use the task id to check the status of the task and retrieve the generated text by sending a GET request to `http://localhost:5000/result/<task_id>`, for example:
+    ```
+    http://localhost:5000/result/8d5f0b5d-a5e5-4b8f-b5f5-5a5a5a5a5a5a
+    ```
+11. To genarate image same process as 7 to 9 just POST request URL is `http://localhost:5000/image_chat` and GET URL is `http://localhost:5000/image/<task_id>`
+
+## Docker
+
+1. Copy example.env to .env and give required variables
+   ```
+   cp example.env .env
+   ```
+2. Build the Docker image by running the following command in terminal:
+   ```
+   docker build -t gpt-chat-flask-celery .
+   ```
+3. Once the image is built, you can run the container using the following command:
+   ```
+   docker run -p 5000:5000 --name gpt-chat-flask-celery gpt-chat-flask-celery
+   ```
+4. You can access the application on `http://localhost:5000`
+
+## Limitations
+
+This sample chatbot has some limitations and is just for demonstration purposes.
+
+- It uses GPT-3 with a single endpoint and does not handle pagination.
+- It does not handle any form of natural language processing, sentiment analysis, or other advanced features.
+- It does not have any form of UI and just accepts and returns json data.
+
+## Contributions
+
+If you find any bugs or would like to add new features, please feel free to open a pull request or an issue.
+
+## License
+
+This repository is released under the MIT license. See [LICENSE](https://github.com/shamspias/chat-gpt-celery-flask/blob/main/LICENSE.md) for more details.
+
+## Test Client
+
+to test this can use this simple client
+
+[React-Client](https://github.com/shamspias/chat-gpt-react-client)
---
+++ server/app.py
@@ -0,0 +1,121 @@
+import os
+import requests
+from dotenv import load_dotenv
+from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
+from celery import Celery
+import openai
+
+app = Flask(__name__)
+CORS(app)
+load_dotenv()
+
+# Configure Celery here we use radis
+app.config['CELERY_BROKER_URL'] = os.getenv('CELERY_BROKER_URL')
+app.config['CELERY_RESULT_BACKEND'] = os.getenv('CELERY_RESULT_BACKEND')
+
+celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
+
+# GPT-3 endpoint and credentials
+gpt3_endpoint = "https://api.openai.com/v1/engines/text-davinci-003/completions"
+gpt3_image_endpoint = "https://api.openai.com/v1/images/generations"
+gpt3_api_key = os.getenv("OPEN_AI_KEY")
+openai.api_key = gpt3_api_key
+
+
+@celery.task
+def generate_text(prompt):
+    # Send request to GPT-3
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {gpt3_api_key}"
+    }
+    data = {
+        "prompt": prompt,
+        # "temperature": 0.5,
+        # "max_tokens": 128
+    }
+    response = requests.post(gpt3_endpoint, headers=headers, json=data)
+
+    # Return response
+    return response.json()
+
+
+@celery.task
+def generate_image(prompt, number, image_size, image_width):
+    response = openai.Image.create(
+        prompt=prompt,
+        n=number,
+        size=str(image_size) + "x" + str(image_width)
+    )
+    image_url = response['data']
+
+    return image_url
+
+
+@app.route('/', methods=['GET'])
+def ping():
+    return jsonify({'message': "server is running", "status": True})
+
+@app.route('/chat-async', methods=['POST'])
+def chatAsync():
+    # Get prompt from client
+    prompt = request.json.get('prompt')
+
+    # Run GPT-3 task asynchronously
+    task = generate_text.apply_async(args=(prompt,))
+
+    # Return task id
+    return jsonify({'task_id': task.id})
+
+@app.route('/chat-sync', methods=['POST'])
+def chatSync():
+    # Get prompt from client
+    prompt = request.json.get('prompt')
+
+    # Run GPT-3 task asynchronously
+    response = generate_text(prompt)
+    result = response['choices'][0]['text']
+    # Return task id
+    return jsonify({'reply': result})
+
+@app.route('/result/<task_id>', methods=['GET'])
+def result(task_id):
+    # Get task result
+    response = generate_text.AsyncResult(task_id).get()
+    result = response['choices'][0]['text']
+    result = result[2:]
+    # Return response
+    return jsonify({
+        "result": result
+    })
+
+@app.route('/image_chat', methods=['POST'])
+def image_chat():
+    # Get prompt from client
+    prompt = request.json.get('prompt', "cartoon cat")
+    number = request.json.get('number', 1)
+    image_size = request.json.get('image_size', 1024)
+    image_width = request.json.get('image_width', 1024)
+
+    # Run GPT-3 task asynchronously
+    task = generate_image.apply_async(args=(prompt, number, image_size, image_width))
+
+    # Return task id
+    return jsonify({'task_id': task.id})
+
+
+@app.route('/image/<task_id>', methods=['GET'])
+def image_result(task_id):
+    # Get task result
+    response = generate_image.AsyncResult(task_id).get()
+
+    # Return response
+    return jsonify({
+        "data": response
+    })
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000, debug=True)
